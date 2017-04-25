33895e8e331afd4f065648a270c6b3107c038423
ddivney <daniel.divney@gmail.com>
2017-04-25 16:01:13 -0600

    Sirve, implementado Pair<url, type>

---
+++ Web Crawler/src/crawler/Pair.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crawler;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Pair<U, T> {
+    public final U u;
+    public final T t;
+    public Pair(U u, T t){
+        this.u = u;
+        this.t = t;
+    }
+}
